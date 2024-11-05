1afbfe36c20d98de6f3cd2753ebe416fe766cf1f
nareshgantala <nareshgantala@gmail.com>
2024-11-05 07:56:50 +0530

    pass

--- setup_web.tf
+++ setup_web.tf
@@ -16,7 +16,7 @@ output "instance_public_ip" {
 resource "null_resource" "ansible_playbook" {
   provisioner "local-exec" {
     command = <<-EOF
-      sshpass -p "your_password" ssh -o StrictHostKeyChecking=no centos@${data.aws_instances.existing_instance.public_ips[0]} << 'EOSSH'
+      sshpass -p "DevOps321" ssh -o StrictHostKeyChecking=no centos@${data.aws_instances.existing_instance.public_ips[0]} << 'EOSSH'
         cd /home/centos/ejs_project_2
         docker build -t my-node-app .
         docker run -d -p 3000:3000 my-node-app
